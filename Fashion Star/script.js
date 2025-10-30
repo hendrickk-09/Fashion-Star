7f0ef0830dfe546f601973c7a35f56cff6f663ad
hendrickk-09 <hendricklroncoski@gmail.com>
2025-10-30 08:56:03 -0300

    Catalogo Urbano

--- Fashion Star/script.js
+++ Fashion Star/script.js
@@ -1,9 +1,9 @@
 let show = true;
 const menuContent = document.querySelector('.content');
 const menuToggle = menuContent.querySelector('.menu-toggle');
-const carrinho = document.querySelector(".fas fa-shopping-cart");
-const caixa = document.querySelector(".caixa-carrinho");
-
+const carrinho = document.querySelector('.carrinho')
+const caixacarrinho = document.getElementById('caixacarrinho')
+const cancelar = document.getElementById('cancelar-compra')
 
 
 menuToggle.addEventListener('click', () => {
@@ -13,3 +13,11 @@ menuToggle.addEventListener('click', () => {
     menuContent.classList.toggle('on', show);
     show = !show;
 })
+
+carrinho.addEventListener('click', () => {
+    caixacarrinho.style.display = 'block';
+});
+
+cancelar.addEventListener('click', () => {
+    caixacarrinho.style.display = 'none';
+});
